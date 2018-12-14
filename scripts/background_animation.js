683d19d9fe3365a1ddf1b76d56a7d19e08672e59
Zac Bolton <zacbolton2129@gmail.com>
2018-12-14 19:17:30 -0200

    transferring to hardcoded html controls

--- scripts/background_animation.js
+++ scripts/background_animation.js
@@ -37,39 +37,39 @@ var isTouch = ('ontouchstart' in window);
 
 // console.log(window)
 
-function kill(type){
- window.addEventListener(type, function(e){
-   e.preventDefault();
-   e.stopPropagation();
-   return false;
- }, true);
-}
+// function kill(type){
+//  window.addEventListener(type, function(e){
+//    e.preventDefault();
+//    e.stopPropagation();
+//    return false;
+//  }, true);
+// }
 
-if( isTouch ){
- kill('mousedown');
- kill('mousemove');
-}
+// if( isTouch ){
+//  kill('mousedown');
+//  kill('mousemove');
+// }
 
-window.addEventListener('touchend', handleMouseUp);
-window.addEventListener('mouseup', handleMouseUp);
+// window.addEventListener('touchend', handleMouseUp);
+// window.addEventListener('mouseup', handleMouseUp);
 
-function handleMouseUp() {
-    nodeDecIsDown = false;
-    nodeIncIsDown = false;
-    factorDecIsDown = false;
-    factorIncIsDown = false;
-    long = false;
+// function handleMouseUp() {
+//     nodeDecIsDown = false;
+//     nodeIncIsDown = false;
+//     factorDecIsDown = false;
+//     factorIncIsDown = false;
+//     long = false;
 
-    setTimeout(function(){
-        nodeDecUserInput.style('border-right', '2rem solid #1A1A1A');
-        nodeIncUserInput.style('border-left', '2rem solid #1A1A1A')
-        nodeUserOutput.style('color', '#1A1A1A');
+//     setTimeout(function(){
+//         nodeDecUserInput.style('border-right', '2rem solid #1A1A1A');
+//         nodeIncUserInput.style('border-left', '2rem solid #1A1A1A')
+//         nodeUserOutput.style('color', '#1A1A1A');
 
-        factorDecUserInput.style('border-right', '2rem solid #1A1A1A');
-        factorIncUserInput.style('border-left', '2rem solid #1A1A1A')
-        factorUserOutput.style('color', '#1A1A1A');
-    }, highlightTimeout);
-}
+//         factorDecUserInput.style('border-right', '2rem solid #1A1A1A');
+//         factorIncUserInput.style('border-left', '2rem solid #1A1A1A')
+//         factorUserOutput.style('color', '#1A1A1A');
+//     }, highlightTimeout);
+// }
 
 function windowResized()
 {
@@ -97,8 +97,8 @@ function windowResized()
 function newAnimation(nodes, factor, ui = false)
 {
     setTimeout(function(){
-        newAnimationButton.remove();
-        animationControlsContainer.remove();
+        // newAnimationButton.remove();
+        // animationControlsContainer.remove();
         canvas.remove();
 
         setup(nodes, factor);
@@ -121,34 +121,34 @@ function setup(nodes, factor) {
 };
 
 function draw() {
-    if (nodeDecIsDown && (nodes > minNodes) && long){
-        nodeDecUserInput.style('border-right', '2rem solid white');
-        nodeUserOutput.style('color', 'white');
-        nodes --;
-        nodeUserOutput.html(nodes);
-        newAnimation(nodes, factor, true);
-    }
-    if (nodeIncIsDown && (nodes < maxNodes) && long){
-        nodeIncUserInput.style('border-left', '2rem solid white');
-        nodeUserOutput.style('color', 'white');
-        nodes ++;
-        nodeUserOutput.html(nodes);
-        newAnimation(nodes, factor, true);
-    }
-    if (factorDecIsDown && (factor > minFactor) && long){
-        factorDecUserInput.style('border-right', '2rem solid white');
-        factorUserOutput.style('color', 'white');
-        factor --;
-        factorUserOutput.html(factor);
-        newAnimation(nodes, factor, true);
-    }
-    if (factorIncIsDown && (factor < maxNodes) && long){
-        factorIncUserInput.style('border-left', '2rem solid white');
-        factorUserOutput.style('color', 'white');
-        factor ++;
-        factorUserOutput.html(factor);
-        newAnimation(nodes, factor, true);
-    }
+    // if (nodeDecIsDown && (nodes > minNodes) && long){
+    //     nodeDecUserInput.style('border-right', '2rem solid white');
+    //     nodeUserOutput.style('color', 'white');
+    //     nodes --;
+    //     nodeUserOutput.html(nodes);
+    //     newAnimation(nodes, factor, true);
+    // }
+    // if (nodeIncIsDown && (nodes < maxNodes) && long){
+    //     nodeIncUserInput.style('border-left', '2rem solid white');
+    //     nodeUserOutput.style('color', 'white');
+    //     nodes ++;
+    //     nodeUserOutput.html(nodes);
+    //     newAnimation(nodes, factor, true);
+    // }
+    // if (factorDecIsDown && (factor > minFactor) && long){
+    //     factorDecUserInput.style('border-right', '2rem solid white');
+    //     factorUserOutput.style('color', 'white');
+    //     factor --;
+    //     factorUserOutput.html(factor);
+    //     newAnimation(nodes, factor, true);
+    // }
+    // if (factorIncIsDown && (factor < maxNodes) && long){
+    //     factorIncUserInput.style('border-left', '2rem solid white');
+    //     factorUserOutput.style('color', 'white');
+    //     factor ++;
+    //     factorUserOutput.html(factor);
+    //     newAnimation(nodes, factor, true);
+    // }
 
     translate(windowWidth/2, windowHeight/2);
 
@@ -186,8 +186,8 @@ function draw() {
 
 function animationControls(udnodes, udfactor)
 {
-    newAnimationButton = createButton('New Animation').addClass('new-animation-button');
-    newAnimationButton.mousePressed(newAnimation);
+    // newAnimationButton = createButton('New Animation').addClass('new-animation-button');
+    // newAnimationButton.mousePressed(newAnimation);
     if (!udnodes)
     {
         nodes = ceil(random(minNodes, maxNodes));
@@ -198,126 +198,126 @@ function animationControls(udnodes, udfactor)
         factor = ceil(random(minFactor, maxFactor));
     }
 
-    animationControlsContainer = createDiv().addClass('animation-controls-container');
+    // animationControlsContainer = createDiv().addClass('animation-controls-container');
 
-    nodeControlsContainer = createDiv().addClass('node-controls-container');
-    nodeControlsContainer.parent(animationControlsContainer);
+    // nodeControlsContainer = createDiv().addClass('node-controls-container');
+    // nodeControlsContainer.parent(animationControlsContainer);
 
-    factorControlsContainer = createDiv().addClass('factor-contols-container');
-    factorControlsContainer.parent(animationControlsContainer);
+    // factorControlsContainer = createDiv().addClass('factor-contols-container');
+    // factorControlsContainer.parent(animationControlsContainer);
 
     // =================================
     // ========= NODE CONTROLS =========
     // =================================
-    nodeDecUserInput = createDiv().addClass('input decrement node-controls');
-    nodeDecUserInput.parent(nodeControlsContainer);
+    // nodeDecUserInput = createDiv().addClass('input decrement node-controls');
+    // nodeDecUserInput.parent(nodeControlsContainer);
 
-    nodeUserOutput = createDiv(nodes).addClass('output node-controls');
-    nodeUserOutput.parent(nodeControlsContainer);
+    // nodeUserOutput = createDiv(nodes).addClass('output node-controls');
+    // nodeUserOutput.parent(nodeControlsContainer);
 
-    nodeIncUserInput = createDiv().addClass('input increment node-controls');
-    nodeIncUserInput.parent(nodeControlsContainer);
+    // nodeIncUserInput = createDiv().addClass('input increment node-controls');
+    // nodeIncUserInput.parent(nodeControlsContainer);
 
-    nodeDecUserInput.mousePressed(function(e){
-        if ((nodes > minNodes) && !long){
-            currentEvent = e;
-            nodeDecUserInput.style('border-right', '2rem solid white');
-            nodeUserOutput.style('color', 'white');
-            nodeDecIsDown = true;
-            nodes --;
-            nodeUserOutput.html(nodes);
-            newAnimation(nodes, factor, true);
-            setTimeout(function(){
-                if (nodeDecIsDown)
-                {
-                    long = true;
-                }
-                else
-                {
-                    long = false;
-                }
+    // nodeDecUserInput.mousePressed(function(e){
+    //     if ((nodes > minNodes) && !long){
+    //         currentEvent = e;
+    //         nodeDecUserInput.style('border-right', '2rem solid white');
+    //         nodeUserOutput.style('color', 'white');
+    //         nodeDecIsDown = true;
+    //         nodes --;
+    //         nodeUserOutput.html(nodes);
+    //         newAnimation(nodes, factor, true);
+    //         setTimeout(function(){
+    //             if (nodeDecIsDown)
+    //             {
+    //                 long = true;
+    //             }
+    //             else
+    //             {
+    //                 long = false;
+    //             }
                 
-            }, holdTime);
-        }
-    });
-    nodeIncUserInput.mousePressed(function(e, d){
-        console.log(e.target);
-        if ((nodes < maxNodes) && !long){
-            currentEvent = e;
-            nodeIncUserInput.style('border-left', '2rem solid white');
-            nodeUserOutput.style('color', 'white');
-            nodeIncIsDown = true;
-            nodes ++;
-            nodeUserOutput.html(nodes);
-            newAnimation(nodes, factor, true);
-            setTimeout(function(){
-                if (nodeIncIsDown)
-                {
-                    long = true;
-                }
-                else
-                {
-                    long = false;
-                }
+    //         }, holdTime);
+    //     }
+    // });
+    // nodeIncUserInput.mousePressed(function(e, d){
+    //     console.log(e.target);
+    //     if ((nodes < maxNodes) && !long){
+    //         currentEvent = e;
+    //         nodeIncUserInput.style('border-left', '2rem solid white');
+    //         nodeUserOutput.style('color', 'white');
+    //         nodeIncIsDown = true;
+    //         nodes ++;
+    //         nodeUserOutput.html(nodes);
+    //         newAnimation(nodes, factor, true);
+    //         setTimeout(function(){
+    //             if (nodeIncIsDown)
+    //             {
+    //                 long = true;
+    //             }
+    //             else
+    //             {
+    //                 long = false;
+    //             }
                 
-            }, holdTime);
-        }
-    });
+    //         }, holdTime);
+    //     }
+    // });
 
-    // =================================
-    // ======== FACTOR CONTROLS ========
-    // =================================
-    factorDecUserInput = createDiv().addClass('input decrement factor-controls');
-    factorDecUserInput.parent(factorControlsContainer);
+    // // =================================
+    // // ======== FACTOR CONTROLS ========
+    // // =================================
+    // // factorDecUserInput = createDiv().addClass('input decrement factor-controls');
+    // // factorDecUserInput.parent(factorControlsContainer);
 
-    factorUserOutput = createDiv(factor).addClass('output factor-controls');
-    factorUserOutput.parent(factorControlsContainer);
+    // // factorUserOutput = createDiv(factor).addClass('output factor-controls');
+    // // factorUserOutput.parent(factorControlsContainer);
 
-    factorIncUserInput = createDiv().addClass('input increment factor-controls');
-    factorIncUserInput.parent(factorControlsContainer);
+    // // factorIncUserInput = createDiv().addClass('input increment factor-controls');
+    // // factorIncUserInput.parent(factorControlsContainer);
 
-    factorDecUserInput.mousePressed(function(e){
-        if ((factor > minFactor) && !long){
-            currentEvent = e;
-            factorDecUserInput.style('border-right', '2rem solid white');
-            factorUserOutput.style('color', 'white');
-            factorDecIsDown = true;
-            factor --;
-            factorUserOutput.html(factor);
-            newAnimation(nodes, factor, true);
-            setTimeout(function(){
-                if (factorDecIsDown)
-                {
-                    long = true;
-                }
-                else
-                {
-                    long = false;
-                }
+    // factorDecUserInput.mousePressed(function(e){
+    //     if ((factor > minFactor) && !long){
+    //         currentEvent = e;
+    //         factorDecUserInput.style('border-right', '2rem solid white');
+    //         factorUserOutput.style('color', 'white');
+    //         factorDecIsDown = true;
+    //         factor --;
+    //         factorUserOutput.html(factor);
+    //         newAnimation(nodes, factor, true);
+    //         setTimeout(function(){
+    //             if (factorDecIsDown)
+    //             {
+    //                 long = true;
+    //             }
+    //             else
+    //             {
+    //                 long = false;
+    //             }
                 
-            }, holdTime);
-        }
+    //         }, holdTime);
+    //     }
         
-    });
-    factorIncUserInput.mousePressed(function(e){
-        if ((factor < maxNodes) && !long){
-            currentEvent = e;
-            factorIncUserInput.style('border-left', '2rem solid white');
-            factorUserOutput.style('color', 'white');
-            factorIncIsDown = true;
-            factor ++;
-            factorUserOutput.html(factor);
-            newAnimation(nodes, factor, true);
-            setTimeout(function(){
-                if (factorIncIsDown)
-                {
-                    long = true;
-                }
-                else
-                {
-                    long = false;
-                }
-            }, holdTime);
-        }
-    });
+    // });
+    // factorIncUserInput.mousePressed(function(e){
+    //     if ((factor < maxNodes) && !long){
+    //         currentEvent = e;
+    //         factorIncUserInput.style('border-left', '2rem solid white');
+    //         factorUserOutput.style('color', 'white');
+    //         factorIncIsDown = true;
+    //         factor ++;
+    //         factorUserOutput.html(factor);
+    //         newAnimation(nodes, factor, true);
+    //         setTimeout(function(){
+    //             if (factorIncIsDown)
+    //             {
+    //                 long = true;
+    //             }
+    //             else
+    //             {
+    //                 long = false;
+    //             }
+    //         }, holdTime);
+    //     }
+    // });
 };
