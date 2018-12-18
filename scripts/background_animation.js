3bff94d9c18fcb745bce0db81b9d39022aa0e92b
Zac Bolton <zacbolton2129@gmail.com>
2018-12-18 20:43:45 -0200

    scan implemented

--- scripts/background_animation.js
+++ scripts/background_animation.js
@@ -15,25 +15,11 @@ var maxNodes = 4000;
 var minNodes = 9;
 var minFactor = 2;
 var maxFactorRelToNodes = 3;
-var animationControlsContainer;
-var nodeControlsContainer;
-var factorControlsContainer;
-var nodeIncUserInput;
-var nodeDecUserInput;
-var nodeUserOutput;
-var factorIncUserInput;
-var factorDeccUserInput;
-var factorUserOutput;
-var newAnimationButton;
-var nodeDecIsDown = false;
-var nodeIncIsDown = false;
-var factorDecIsDown = false;
-var factorIncIsDown = false;
-var long = false;
-var holdTime = 200;
-var highlightTimeout = 200;
-var currentEvent;
-var isTouch = ('ontouchstart' in window);
+
+var hold = false;
+var down = false;
+var g_isNodes;
+var g_isIncrement;
 
 var colorScheme = 120;
 
@@ -57,41 +43,6 @@ window.onload = function () {
         }
     }
 };
-// console.log(window)
-
-// function kill(type){
-//  window.addEventListener(type, function(e){
-//    e.preventDefault();
-//    e.stopPropagation();
-//    return false;
-//  }, true);
-// }
-
-// if( isTouch ){
-//  kill('mousedown');
-//  kill('mousemove');
-// }
-
-// window.addEventListener('touchend', handleMouseUp);
-// window.addEventListener('mouseup', handleMouseUp);
-
-// function handleMouseUp() {
-//     nodeDecIsDown = false;
-//     nodeIncIsDown = false;
-//     factorDecIsDown = false;
-//     factorIncIsDown = false;
-//     long = false;
-
-//     setTimeout(function(){
-//         nodeDecUserInput.style('border-right', '2rem solid #1A1A1A');
-//         nodeIncUserInput.style('border-left', '2rem solid #1A1A1A')
-//         nodeUserOutput.style('color', '#1A1A1A');
-
-//         factorDecUserInput.style('border-right', '2rem solid #1A1A1A');
-//         factorIncUserInput.style('border-left', '2rem solid #1A1A1A')
-//         factorUserOutput.style('color', '#1A1A1A');
-//     }, highlightTimeout);
-// }
 
 function windowResized()
 {
@@ -122,34 +73,10 @@ function setup(nodes, factor) {
 };
 
 function draw() {
-    // if (nodeDecIsDown && (nodes > minNodes) && long){
-    //     nodeDecUserInput.style('border-right', '2rem solid white');
-    //     nodeUserOutput.style('color', 'white');
-    //     nodes --;
-    //     nodeUserOutput.html(nodes);
-    //     newAnimation(nodes, factor, true);
-    // }
-    // if (nodeIncIsDown && (nodes < maxNodes) && long){
-    //     nodeIncUserInput.style('border-left', '2rem solid white');
-    //     nodeUserOutput.style('color', 'white');
-    //     nodes ++;
-    //     nodeUserOutput.html(nodes);
-    //     newAnimation(nodes, factor, true);
-    // }
-    // if (factorDecIsDown && (factor > minFactor) && long){
-    //     factorDecUserInput.style('border-right', '2rem solid white');
-    //     factorUserOutput.style('color', 'white');
-    //     factor --;
-    //     factorUserOutput.html(factor);
-    //     newAnimation(nodes, factor, true);
-    // }
-    // if (factorIncIsDown && (factor < maxNodes) && long){
-    //     factorIncUserInput.style('border-left', '2rem solid white');
-    //     factorUserOutput.style('color', 'white');
-    //     factor ++;
-    //     factorUserOutput.html(factor);
-    //     newAnimation(nodes, factor, true);
-    // }
+    if (hold) {
+        nodes = nodes + ((g_isNodes * g_isIncrement) || (g_isNodes * -1));
+        factor = factor + ((((g_isNodes - 1) * -1) * g_isIncrement) || (((g_isNodes - 1) * -1) * -1));
+    }
 
     translate(windowWidth/2, windowHeight/2);
 
@@ -190,8 +117,6 @@ function draw() {
 
 function animationControls(udnodes, udfactor)
 {
-    // newAnimationButton = createButton('New Animation').addClass('new-animation-button');
-    // newAnimationButton.mousePressed(newAnimation);
     if (!udnodes)
     {
         nodes = ceil(random(minNodes, maxNodes));
@@ -201,145 +126,55 @@ function animationControls(udnodes, udfactor)
     {
         factor = ceil(random(minFactor, maxFactor));
     }
-
-    // animationControlsContainer = createDiv().addClass('animation-controls-container');
-
-    // nodeControlsContainer = createDiv().addClass('node-controls-container');
-    // nodeControlsContainer.parent(animationControlsContainer);
-
-    // factorControlsContainer = createDiv().addClass('factor-contols-container');
-    // factorControlsContainer.parent(animationControlsContainer);
-
-    // =================================
-    // ========= NODE CONTROLS =========
-    // =================================
-    // nodeDecUserInput = createDiv().addClass('input decrement node-controls');
-    // nodeDecUserInput.parent(nodeControlsContainer);
-
-    // nodeUserOutput = createDiv(nodes).addClass('output node-controls');
-    // nodeUserOutput.parent(nodeControlsContainer);
-
-    // nodeIncUserInput = createDiv().addClass('input increment node-controls');
-    // nodeIncUserInput.parent(nodeControlsContainer);
-
-    // nodeDecUserInput.mousePressed(function(e){
-    //     if ((nodes > minNodes) && !long){
-    //         currentEvent = e;
-    //         nodeDecUserInput.style('border-right', '2rem solid white');
-    //         nodeUserOutput.style('color', 'white');
-    //         nodeDecIsDown = true;
-    //         nodes --;
-    //         nodeUserOutput.html(nodes);
-    //         newAnimation(nodes, factor, true);
-    //         setTimeout(function(){
-    //             if (nodeDecIsDown)
-    //             {
-    //                 long = true;
-    //             }
-    //             else
-    //             {
-    //                 long = false;
-    //             }
-                
-    //         }, holdTime);
-    //     }
-    // });
-    // nodeIncUserInput.mousePressed(function(e, d){
-    //     console.log(e.target);
-    //     if ((nodes < maxNodes) && !long){
-    //         currentEvent = e;
-    //         nodeIncUserInput.style('border-left', '2rem solid white');
-    //         nodeUserOutput.style('color', 'white');
-    //         nodeIncIsDown = true;
-    //         nodes ++;
-    //         nodeUserOutput.html(nodes);
-    //         newAnimation(nodes, factor, true);
-    //         setTimeout(function(){
-    //             if (nodeIncIsDown)
-    //             {
-    //                 long = true;
-    //             }
-    //             else
-    //             {
-    //                 long = false;
-    //             }
-                
-    //         }, holdTime);
-    //     }
-    // });
-
-    // // =================================
-    // // ======== FACTOR CONTROLS ========
-    // // =================================
-    // // factorDecUserInput = createDiv().addClass('input decrement factor-controls');
-    // // factorDecUserInput.parent(factorControlsContainer);
-
-    // // factorUserOutput = createDiv(factor).addClass('output factor-controls');
-    // // factorUserOutput.parent(factorControlsContainer);
-
-    // // factorIncUserInput = createDiv().addClass('input increment factor-controls');
-    // // factorIncUserInput.parent(factorControlsContainer);
-
-    // factorDecUserInput.mousePressed(function(e){
-    //     if ((factor > minFactor) && !long){
-    //         currentEvent = e;
-    //         factorDecUserInput.style('border-right', '2rem solid white');
-    //         factorUserOutput.style('color', 'white');
-    //         factorDecIsDown = true;
-    //         factor --;
-    //         factorUserOutput.html(factor);
-    //         newAnimation(nodes, factor, true);
-    //         setTimeout(function(){
-    //             if (factorDecIsDown)
-    //             {
-    //                 long = true;
-    //             }
-    //             else
-    //             {
-    //                 long = false;
-    //             }
-                
-    //         }, holdTime);
-    //     }
-        
-    // });
-    // factorIncUserInput.mousePressed(function(e){
-    //     if ((factor < maxNodes) && !long){
-    //         currentEvent = e;
-    //         factorIncUserInput.style('border-left', '2rem solid white');
-    //         factorUserOutput.style('color', 'white');
-    //         factorIncIsDown = true;
-    //         factor ++;
-    //         factorUserOutput.html(factor);
-    //         newAnimation(nodes, factor, true);
-    //         setTimeout(function(){
-    //             if (factorIncIsDown)
-    //             {
-    //                 long = true;
-    //             }
-    //             else
-    //             {
-    //                 long = false;
-    //             }
-    //         }, holdTime);
-    //     }
-    // });
 };
 
-function userInput(event, isNodes, isIncrement) {
-    nodes = nodes + ((isNodes * isIncrement) || (isNodes * -1));
-    factor = factor + ((((isNodes - 1) * -1) * isIncrement) || (((isNodes - 1) * -1) * -1));
-    var inEl = event.target;
-    var outEl = (isNodes) ? document.getElementById('nodesOutput') : document.getElementById('factorOutput');
-    waitForHold(inEl, outEl, input_class = (isIncrement) ? 'incFadeout' : 'decFadeout');
-    setup(nodes, factor)
+function userInput(isNodes, isIncrement) {
+    g_isNodes = isNodes;
+    g_isIncrement = isIncrement;
+    down = true;
+    hold = false;
+    // iterate once
+    handleInput(isNodes, isIncrement);
+
+    setTimeout(function(){
+        hold = down ? true : false;
+        // while (hold) {
+        //     scanValues = [isNodes, isIncrement];
+        // }
+    }, 1000);
 }
 
-function waitForHold(inEl, outEl, input_class) {
-    $(inEl).addClass(input_class);
+function handleInput(isNodes, isIncrement) {
+    // update values
+    nodes = nodes + ((isNodes * isIncrement) || (isNodes * -1));
+    factor = factor + ((((isNodes - 1) * -1) * isIncrement) || (((isNodes - 1) * -1) * -1));
+    // get hold of relevant elements
+    var inEl = document.getElementById(isIncrement ? (isNodes ? 'incNodes' : 'incFactor') : (isNodes ? 'decNodes' : 'decFactor'));
+    var outEl = document.getElementById(isNodes ? 'nodesOutput' : 'factorOutput');
+    // make relevant elements white
+    $(inEl).css(isIncrement ? 'border-left' : 'border-right', '2rem solid white');
+    $(outEl).css('color', 'white');
+    // reset animation with new values
+    setup(nodes, factor);
+}
+
+function endUserInput(isNodes, isIncrement) {
+    console.log('endUserInput says hold = '+hold);
+    hold = false;
+    down = false;
+    // get ahold of relevant elements
+    var inEl = document.getElementById(isIncrement ? (isNodes ? 'incNodes' : 'incFactor') : (isNodes ? 'decNodes' : 'decFactor'));
+    var outEl = isNodes ? document.getElementById('nodesOutput') : document.getElementById('factorOutput');
+    // apply fade out
+    $(inEl).addClass(isIncrement ? 'incFadeout' : 'decFadeout');
     $(outEl).addClass('outputFadout');
-    var newone = inEl.cloneNode(true);
+    // create clones
+    var newInEl = inEl.cloneNode(true);
     var newOutEl = outEl.cloneNode(true);
-    inEl.parentNode.replaceChild(newone, inEl);
+    // switch for clones
+    inEl.parentNode.replaceChild(newInEl, inEl);
     outEl.parentNode.replaceChild(newOutEl, outEl);
+    // style clones
+    $(newInEl).css(isIncrement ? 'border-left' : 'border-right', '2rem solid #1A1A1A');
+    $(newOutEl).css('color', '#1A1A1A')
 }
