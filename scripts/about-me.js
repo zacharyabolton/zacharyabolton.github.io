14db9909200164586a2595c744623d8ced510353
zacharyabolton <zacbolton2129@gmail.com>
2023-09-15 15:59:27 -0300

    Stylizes home buttons with iridescent animation; Rewords ASU ESL Cert listing

--- scripts/about-me.js
+++ scripts/about-me.js
@@ -3,7 +3,18 @@ var squareSide;
 var board;
 var game;
 
-var rSprite, nSprite, bSprite, qSprite, kSprite, pSprite, RSprite, NSprite, BSprite, QSprite, KSprite, PSprite;
+var rSprite,
+  nSprite,
+  bSprite,
+  qSprite,
+  kSprite,
+  pSprite,
+  RSprite,
+  NSprite,
+  BSprite,
+  QSprite,
+  KSprite,
+  PSprite;
 var impactFont;
 var movingPiece;
 var sx, sy;
@@ -12,63 +23,63 @@ var tx, ty;
 var whitesMove = true;
 
 function preload() {
-    rSprite = loadImage('../assets/chess_pieces/black_rook.svg');
-    RSprite = loadImage('../assets/chess_pieces/white_rook.svg');
-    nSprite = loadImage('../assets/chess_pieces/black_knight.svg');
-    NSprite = loadImage('../assets/chess_pieces/white_knight.svg');
-    bSprite = loadImage('../assets/chess_pieces/black_bishop.svg');
-    BSprite = loadImage('../assets/chess_pieces/white_bishop.svg');
-    qSprite = loadImage('../assets/chess_pieces/black_queen.svg');
-    QSprite = loadImage('../assets/chess_pieces/white_queen.svg');
-    kSprite = loadImage('../assets/chess_pieces/black_king.svg');
-    KSprite = loadImage('../assets/chess_pieces/white_king.svg');
-    pSprite = loadImage('../assets/chess_pieces/black_pawn.svg');
-    PSprite = loadImage('../assets/chess_pieces/white_pawn.svg');
-    impactFont = loadFont('../assets/Impact.ttf')
+  rSprite = loadImage("../assets/chess_pieces/black_rook.svg");
+  RSprite = loadImage("../assets/chess_pieces/white_rook.svg");
+  nSprite = loadImage("../assets/chess_pieces/black_knight.svg");
+  NSprite = loadImage("../assets/chess_pieces/white_knight.svg");
+  bSprite = loadImage("../assets/chess_pieces/black_bishop.svg");
+  BSprite = loadImage("../assets/chess_pieces/white_bishop.svg");
+  qSprite = loadImage("../assets/chess_pieces/black_queen.svg");
+  QSprite = loadImage("../assets/chess_pieces/white_queen.svg");
+  kSprite = loadImage("../assets/chess_pieces/black_king.svg");
+  KSprite = loadImage("../assets/chess_pieces/white_king.svg");
+  pSprite = loadImage("../assets/chess_pieces/black_pawn.svg");
+  PSprite = loadImage("../assets/chess_pieces/white_pawn.svg");
+  impactFont = loadFont("../assets/Impact.ttf");
 }
 
 function setup() {
-    boardSide = min(windowWidth, 700);
-    squareSide = boardSide/8;
-    board = createCanvas(boardSide, boardSide);
+  boardSide = min(windowWidth, 700);
+  squareSide = boardSide / 8;
+  board = createCanvas(boardSide, boardSide);
 
-    game = new Game()
+  game = new Game();
 }
 
 function windowResized() {
-    boardSide = min(windowWidth, 700);
-    squareSide = boardSide/8;
-    board = createCanvas(boardSide, boardSide);
-    game.resize();
+  boardSide = min(windowWidth, 700);
+  squareSide = boardSide / 8;
+  board = createCanvas(boardSide, boardSide);
+  game.resize();
 }
 
 function draw() {
-    game.show();
+  game.show();
 }
 
 function mousePressed(e) {
-    sx = floor(mouseX/squareSide);
-    sy = floor(mouseY/squareSide);
-    movingPiece = game.pieceAt(sx, sy);
-    if (movingPiece && movingPiece.white === whitesMove) {
-        e.preventDefault();
-        movingPiece.movingThisPiece = true;
-    } else {
-        movingPiece = null;
-        sx = null;
-        sy = null;
-    }
-}
-
-function mouseReleased() {
-    if (movingPiece) {
-        tx = floor(mouseX/squareSide);
-        ty = floor(mouseY/squareSide);
-        game.move(tx, ty, sx, sy);
-    }
+  sx = floor(mouseX / squareSide);
+  sy = floor(mouseY / squareSide);
+  movingPiece = game.pieceAt(sx, sy);
+  if (movingPiece && movingPiece.white === whitesMove) {
+    e.preventDefault();
+    movingPiece.movingThisPiece = true;
+  } else {
     movingPiece = null;
     sx = null;
     sy = null;
-    tx = null;
-    ty = null;
+  }
+}
+
+function mouseReleased() {
+  if (movingPiece) {
+    tx = floor(mouseX / squareSide);
+    ty = floor(mouseY / squareSide);
+    game.move(tx, ty, sx, sy);
+  }
+  movingPiece = null;
+  sx = null;
+  sy = null;
+  tx = null;
+  ty = null;
 }
